adab6fa58b035daea9109a51b0594112cfeb095e
Du, Lynn <lindu@ebay.com>
2019-01-25 13:38:51 -0800

    demo

--- src/main/java/HelloAgain.java
+++ src/main/java/HelloAgain.java
@@ -2,5 +2,6 @@ public class HelloAgain {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("Trigger Jenkins automatically!");
+        System.out.println("demo");
     }
 }
